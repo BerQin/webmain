7d2dd70b6f3c915f492df96680c38af4d63d7338
BerQin <berqin@outlook.com>
2020-04-27 14:23:33 +0800

    绣花

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,7 +8,7 @@ class Index extends Controller
     public function index()
     {
         $this -> assign([
-            'name' => '迈特洗碗机 - MATE ELECTRICAL',
+            'name' => '广州迈特洗碗机-全自动洗碗机_大型洗碗机流水线厂家_洗碗机租赁',
             'bodyClassName' => 'page-16 pagelevel-0 language-4 backendlayout-pagets__home layout-0 theme-default mobilemenu--nopush',
             'hasBackPath' => null
         ]);
